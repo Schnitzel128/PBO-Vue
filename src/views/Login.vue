66b5baac48418355442fc20a221b239784fc711b
Martin List <martinlist@me.com>
2018-11-19 19:05:34 +0100

    Adds Login.vue and new route

---
+++ src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-container id="loginForm">
+    <div>
+      <b-form @submit="onSubmit">
+        <b-form-input 
+          v-model="username"
+          type="text"
+          placeholder="Username"
+        />
+        <b-form-input 
+          v-model="password"
+          type="password"
+          placeholder="Password"
+        />  
+        <b-button 
+          id="submitButton" 
+          type="submit"
+        >Login</b-button>
+      </b-form>
+    </div>
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Login",
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert("Username: " + this.username + " Password: " + this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#submitButton {
+  margin: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+#loginForm {
+  width: 30vw;
+}
+</style>
