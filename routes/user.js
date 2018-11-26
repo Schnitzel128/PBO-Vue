1772ae93390cef240a1edd4160e02029f4ca49fc
Unknown <schnitzel128@gmail.com>
2018-11-26 16:21:20 +0100

    Delete Users added

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ require("../auth/passport")(passport);
 // we want to protect this route
 router.get(
   "/",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   async function(req, res) {
     try {
       if (typeof req.query.id !== "undefined") {
@@ -24,5 +24,19 @@ router.get(
     }
   }
 );
+
+router.delete("/:username", async function(req, res, next) {
+  try {
+    const user = await userDB.getUserByUsername(req.params.username);
+    if (user.length === 1) {
+      const deleted = await userDB.deleteUserById(user[0].id);
+      res.send("Deleted " + deleted + " rows.");
+    } else {
+      res.status(400).send("No user found with that username");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
